86679d47b16be8663164473c69b77eb44c0079ab
Fulano de Talanderson@e2etreinamentos.com.br <fulanodetal@exemplo.branderson@e2etreinamentos.com.br>
2020-07-30 00:34:21 -0300

    atualização projeto

---
+++ git-jenkins/src/test/java/Teste.java
@@ -0,0 +1,32 @@
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Teste {
+
+	WebDriver driver;
+	
+	
+	@Before
+	public void setUp() throws Exception {
+		
+		System.setProperty("webdriver.chrome.driver", "c://Driver1/chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.get("https://www.google.com.br");
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		driver.quit();
+	}
+
+	@Test
+	public void test() {
+		
+	}
+
+}
